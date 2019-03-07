af2092ba79e8e4c0809ba1781159bb5b1fb35dca
chenyupeng <chenyupeng@zsoft.com.cn>
2019-03-07 23:23:44 +0800

    补充字段、查询方法

--- src/main/java/com/zy/mapper/CrewServiceMapper.java
+++ src/main/java/com/zy/mapper/CrewServiceMapper.java
@@ -6,11 +6,11 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface CrewServiceMapper {
 
-    @Insert("INSERT into crew_service (create_time,status,name,order,service_logo_url,service_pic_url,description) values (#{createTime},#{status},#{name},#{order},#{serviceLogoUrl},#{servicePicUrl},#{description})")
+    @Insert("INSERT into crew_service (create_time,status,name,order,service_logo_url,service_pic_url,description,nameEn,descriptionEn) values (#{createTime},#{status},#{name},#{order},#{serviceLogoUrl},#{servicePicUrl},#{description},#{nameEn},#{descriptionEn})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     public int insertCrewService(CrewService crewService);
 
-    @Update("UPDATE crew_service SET create_time=#{createTime},status=#{status},name=#{name},order=#{order},service_logo_url=#{serviceLogoUrl},service_pic_url=#{servicePicUrl},description=#{description} where id=#{id}")
+    @Update("UPDATE crew_service SET create_time=#{createTime},status=#{status},name=#{name},order=#{order},service_logo_url=#{serviceLogoUrl},service_pic_url=#{servicePicUrl},description=#{description},nameEn=#{nameEn},descriptionEn=#{descriptionEn} where id=#{id}")
     public int updateCrewService(CrewService crewService);
 
     @Delete("delete from crew_service where id=#{id}")
